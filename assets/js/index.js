04e520d685b0321629cfdf421f662a4eedd70977
Mai Monzón <maimonzon@outlook.com>
2021-12-28 12:54:50 -0300

    escribir la lista de productos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,35 @@
-import {productos} from "./data.js"
+//import {productos} from "./data.js"
+import { Carrito } from "./script.js"
+import { Producto } from "./producto.js"
 
-for (const producto of productos) {
-    console.log(producto)
-}
+const carrito = new Carrito(productos)
+//console.log(carrito)
+
+
+const producto1 = new Producto(
+    1,
+    "valija", 
+    20500, 
+    true);
+
+const producto2 = new Producto (
+    2,
+    "carry on", 
+    15000, 
+    true);
+
+const producto3 = new Producto (
+    3,
+    "handbag",
+    9000, 
+    true);
+
+const producto4 = new Producto (
+    4, 
+    "mochila", 
+    7500, 
+    true);
+
+carrito.agregarItem(producto1)
+carrito.agregarItem(producto2)
+carrito.agregarItem(producto3)
