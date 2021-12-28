723fa70a2d02f8f28a587e7f4bcb5ca055fe746e
Mai Monzón <maimonzon@outlook.com>
2021-12-28 14:24:43 -0300

    intento de trabajar HTML en JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,12 @@
 //import {productos} from "./data.js"
 import { Carrito } from "./script.js"
 import { Producto } from "./producto.js"
+import { renderList } from "./utils.js";
 
-const carrito = new Carrito(productos)
+const carrito = new Carrito();
 //console.log(carrito)
 
-
+/*
 const producto1 = new Producto(
     1,
     "valija", 
@@ -33,3 +34,8 @@ const producto4 = new Producto (
 carrito.agregarItem(producto1)
 carrito.agregarItem(producto2)
 carrito.agregarItem(producto3)
+*/
+
+console.log(carrito)
+const productos = carrito;
+renderList('productos-lista', productos)
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,10 +1,13 @@
-export const renderList = (lista) => {
+export const renderList = (listaId, lista) => {
+
+    const listaContainer = document.getElementById(listaId)
 
     for (const item of lista) {
         const itemList = document.createElement('li')
 
         itemList.innerText = item
 
+        listaContainer.appendChild(itemList)
 
     }
 
